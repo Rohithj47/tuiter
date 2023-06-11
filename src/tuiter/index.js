5c2946267320885bf3109b8bff33477d62720e92
Rohith <rohithjanardhan98@gmail.com>
2023-06-11 19:40:43 -0400

    a5

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -4,16 +4,20 @@ import NavigationSidebar from "./navigation-sidebar";
 import HomeScreen from "./home-screen";
 import ExploreScreen from "./explore-screen";
 import BookmarkScreen from "./bookmarks-screen";
-import ProfileScreen from "./profile-screen";
+// import ProfileScreen from "./profile-screen";
 import WhoToFollow from "./who-to-follow-list";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import whoReducer from "./reducers/who-reducer";
 import tuitsReducer from "./reducers/tuits-reducer";
 import fullTuitsReducer from "./reducers/home-tuits-reducer"
+import ProfileScreen from "./user/profile-screen.js";
+import LoginScreen from "./user/login-screen.js";
+import RegisterScreen from "./user/register-screen";
+import authReducer from "./reducers/auth-reducer";
 
 const store = configureStore({
-   reducer: {who: whoReducer, tuits: tuitsReducer, fullTuits: fullTuitsReducer}
+   reducer: {who: whoReducer, tuits: tuitsReducer, fullTuits: fullTuitsReducer, user: authReducer}
 })
 function Tuiter() {
     return(
@@ -26,6 +30,9 @@ function Tuiter() {
                </div>
                <div className="col-7">
                   <Routes>
+                     <Route path="/login"    element={<LoginScreen    />} />
+                     <Route path="/register" element={<RegisterScreen />} />
+                     <Route path="/profile"  element={<ProfileScreen  />} />
                      <Route path="/home" element={<HomeScreen />}></Route>
                      <Route path="/profile" element={<ProfileScreen />}></Route>
                      <Route path="/bookmarks" element={<BookmarkScreen />}></Route>
